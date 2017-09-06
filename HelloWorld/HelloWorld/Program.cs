5a4310aa7bfcff11a68f95b195eb661fc56beefc
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-09-06 08:24:18 -0400

    Initial commit; includes first two exersises

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "Hello World";
+            Console.WriteLine(testString);
+            Console.Beep();
+        }
+    }
+}
---
+++ HelloWorldFirstAssignment/HelloWorldFirstAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldFirstAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bobby McKenna");
+            Console.WriteLine("My chromebook needs to be rooted to run visual studio :(");  
+        }
+    }
+}
