9538226d8fa76ab5203dc0813276c982e736d5b5
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-09-08 09:19:27 -0400

    modified:   EZCharMaker/.vs/EZCharMaker/v15/.suo 	modified:   VariablesAndDataTypes/.vs/VariablesAndDataTypes/v15/.suo 	modified:   VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
    	VariablesAndDataTypes.zip
    	VariablesAndDataTypes/VariablesAndDataTypes/bin/
    	VariablesAndDataTypes/VariablesAndDataTypes/obj/Debug/VariablesAndDataTypes.csproj.FileListAbsolute.txt
    	VariablesAndDataTypes/VariablesAndDataTypes/obj/Debug/VariablesAndDataTypes.csprojResolveAssemblyReference.cache
    	VariablesAndDataTypes/VariablesAndDataTypes/obj/Debug/VariablesAndDataTypes.exe
    	VariablesAndDataTypes/VariablesAndDataTypes/obj/Debug/VariablesAndDataTypes.pdb

--- VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace VariablesAndDataTypes
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Reformatted bank to
+			string name = "Name: Zera Straxus"; 
+			const double startingPoints = 50;
+			string role = "Class: Wizard";
+			double dex = startingPoints * .2;
+			double str = startingPoints * .2 * .5;
+			double intel = 7;
+			double con = (startingPoints * .2 + startingPoints * .2 * .5) - 2;
+			double cha = (startingPoints - str - dex - intel - con);
+			double tot = dex + str + intel + con + cha;
+
+			// Prints Everything
+			Console.WriteLine(role);
+			Console.WriteLine();
+			Console.WriteLine(name);
+			Console.WriteLine("Dex: " + dex);
+			Console.WriteLine("Str: " + str);
+			Console.WriteLine("Int: " + intel);
+			Console.WriteLine("Con: " + con);
+			Console.WriteLine("Cha: " + cha);
+			Console.WriteLine();
+			Console.WriteLine("Total: " + tot);
+		}
+	}
 }
