f073917ca36b5ecadef99f82471e928e604adcb4
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-10-06 09:30:18 -0400

    Signed-off-by: Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>

---
+++ ExamPractical1/ExamPractical1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPractical1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//initialization of var
+			int goldCurrent = 0;
+			bool badGoldAttempt = false;
+
+			Console.WriteLine("Hello Adventurer!\n\n What is your name?\n");
+			string name = Console.ReadLine().Trim().ToUpper();
+			Console.WriteLine("\nWhy hello " + name + " it's good to meet you.\n\nHow much gold do you have on you right now?");
+
+			do
+			{
+				if (badGoldAttempt == true)
+				{
+					Console.WriteLine("Surely you must be richer than that, I'd wager you have at least 100 gold on you right now.");
+				}
+				else
+				{
+					//encountered a logic error that is solved by having these brackets here
+				}
+
+				goldCurrent = int.Parse(Console.ReadLine());
+				badGoldAttempt = true;
+			}
+			while (!(goldCurrent >= 100));
+
+
+			//First item purchace
+			Console.WriteLine("Since you're going on a quest, you'll need a weapon. " +
+				"I recommend an iron broadsword, it's the iron standard for cutting things!");
+			Console.WriteLine("\nYou buy the iron broadsword.\n\nYou now have " + 
+				(goldCurrent / 2) + " remaining gold pieces.");
+			goldCurrent = goldCurrent / 2;
+
+			//Second Item Purchase
+			Console.WriteLine("If you get into trouble out in the middle of nowhere, you'll " +
+				"need some way to heal up. This potion will restore you to your full strength.");
+			Console.WriteLine("\nYou buy the potion.\n\nYou now have " + 
+				(goldCurrent - (goldCurrent/4)) + " remaining gold pieces");
+			goldCurrent = goldCurrent - (goldCurrent / 4);
+
+			//Third item purchase
+			if (goldCurrent >= 375)
+			{
+				Console.WriteLine("I see you still have a lot of extra gold so let me show you one of our most powerful items,\n\nTHE TOME OF THE ANCIENTS\n\n" +
+					"This powerful artifact can demolish your enemies in one word and today it's only 375 gold pieces!");
+				Console.WriteLine("\nYou buy the TOME OF THE ANCIENTS.\n\nYou now have " +
+				(goldCurrent - 375) + " remaining gold pieces");
+				goldCurrent = goldCurrent - 375;
+			}
+			else
+			{
+				Console.WriteLine("If you had more money, I could offer you this Tome of the Ancient " +
+					"Ones, but it looks like the price is too high for you right now.");
+			}
+
+			//end scenarios
+			Console.WriteLine("Thanks for shopping at Lei's Armory and Library!");
+			if (goldCurrent > 0)
+			{
+				Console.WriteLine("You managed to excape shopping with " + goldCurrent + " gold pieces remaining, Good Job!");
+			}
+			else
+			{
+				Console.WriteLine("That shopkeeper took you for everything you had, you have no gold remaining.");
+			}
+
+		}
+	}
+}
---
+++ RNGandTryParse/RNGandTryParse/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNGandTryParse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rng = new Random();
+			int playerNum = -1;
+			int targetNum = rng.Next(0, 101);
+			bool swapOut = false;
+
+			for (int i = 0; i < 8; i++)
+			{
+				swapOut = false;
+				while (swapOut == false)
+				{
+					Console.Write("Try " + (i + 1) + ": Enter a number between 0 and 100: ");
+
+					//janky yet effective way to check if it's a valid input
+					if (int.TryParse(Console.ReadLine(), out playerNum)) 
+					{
+						if (playerNum <= 100 && playerNum >= 0)
+						{
+							swapOut = true; //excapes the 2nd layer of while loop to progress the program
+						}
+						else
+						{
+							Console.WriteLine("That's not a valid number");
+						}
+					}
+					else
+					{
+						Console.WriteLine("That's not a valid number");
+					}
+				}
+				
+				//checks the number against the randomly generated one
+				if (playerNum < targetNum)
+				{
+					Console.WriteLine("Your guess was lower than the target.");
+				}
+				else if (playerNum > targetNum)
+				{
+					Console.WriteLine("Your guess was higher than the target");
+				}
+				else
+				{
+					Console.WriteLine("Congratulations! You guessed " + targetNum);
+					Console.WriteLine("Thanks for playing!");
+					System.Environment.Exit(0);
+				}
+
+			
+
+			}
+			Console.WriteLine("Sorry, you used up all your guesses!\n\nThe target number was " + targetNum);
+			Console.WriteLine("Game Over!");
+		}
+	}
+}
