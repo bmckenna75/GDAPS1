5390b43b2822e0d4dbfd021216e46992f629a988
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-10-18 09:57:02 -0400

    homework rubric and returing things from methods
    Signed-off-by: Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>

---
+++ ReturningThings/ReturningThings/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturningThings
+{
+	class Book
+	{
+		private string name;
+		private string author;
+		private int numberOfPages;
+		private int pagesRead = 0;
+		private int totalReads = 0;
+
+		//constructor
+		public Book(string n, string a, int p)
+		{
+			name = n;
+			author = a;
+			numberOfPages = p;
+		}
+
+		//Standard Methods
+		public void ReadPages(int pagesToRead)
+		{
+			if (pagesToRead > 0)
+			{
+				pagesRead = pagesRead + pagesToRead;
+				while (pagesRead >= numberOfPages)
+				{
+					pagesRead = pagesRead - numberOfPages;
+					totalReads += 1;
+				}
+			}
+
+		}
+
+		public double TimeToFinishReading(double MinutesPerPage)
+		{
+			int pagesLeft = numberOfPages - pagesRead;
+			return pagesLeft * MinutesPerPage;
+		}
+
+		public string BookReport()
+		{
+			return name + " by " + author + " has " + numberOfPages + " page(s).  You are " + pagesRead + " page(s) in, and have read the book " + totalReads + " time(s).";
+		}
+
+
+	}
+}
---
+++ ReturningThings/ReturningThings/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturningThings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string whatToDo;
+			int excape = 0;
+			Book TheHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 312);
+
+
+
+			do
+			{
+				Console.WriteLine("What would you like to do today? (read/report/time/quit)");
+				whatToDo = Console.ReadLine().Trim().ToLower();
+
+				switch (whatToDo)
+				{
+					case "read":
+						Console.WriteLine("How many pages do you want to read?");
+						TheHobbit.ReadPages(int.Parse(Console.ReadLine()));
+						break;
+					case "report":
+						Console.WriteLine(TheHobbit.BookReport());
+						break;
+					case "time":
+						Console.WriteLine("Please enter the time it takes for you to read one page.");
+						Console.WriteLine("It will take you " + 
+							TheHobbit.TimeToFinishReading(double.Parse(Console.ReadLine())) 
+							+ " minutes to finish the book");
+						break;
+					case "quit":
+						excape = 1;
+						break;
+
+
+				}
+			}
+			while (excape != 1);
+		}
+	}
+}
