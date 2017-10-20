6cac60947d13e3c892fd6a0ed21de66b3150a31d
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-10-20 09:04:52 -0400

    start of hw4
    Signed-off-by: Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>

---
+++ Monopoly/Monopoly/Dice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+	class Dice
+	{
+
+		private Random rng;
+
+		public Dice()
+		{
+			rng = new Random();
+		}
+
+		//Roll Tide! Roll Tide!
+		public int RollDie()
+		{
+			return rng.Next(1, 7);
+		}
+
+
+		public int RollDice(int n)
+		{
+			int rollTotal = 0;
+			for (int i = 0; i < n; i++)
+				{
+				rollTotal = rollTotal + RollDie();
+				}
+			return rollTotal;
+		}	
+
+
+	}
+}
