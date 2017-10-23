02d894b59b43d35ca636e7909b184cfce9dba7ee
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-10-23 09:38:05 -0400

    encapsulation
    Signed-off-by: Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>

---
+++ Encapsulation/Encapsulation/Book.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int totalPages;
+        private string owner;
+        private int timesRead;
+
+        //get only properties
+        public string Title
+        {
+            get { return title; }
+        }
+        public string Author
+        {
+            get { return author; }
+        }
+        public int TotalPages
+        {
+            get { return totalPages; }
+        }
+
+
+        //set && get properties
+        public string Owner
+        {
+            get { return owner; }
+            set
+            {
+                if (value != null && value.Trim().Length > 0)
+                {
+                    owner = value;
+                }
+            }
+        }
+        public int TimesRead
+        {
+            get { return timesRead; }
+            set
+            {
+                if (value > timesRead)
+                {
+                    timesRead = value;
+                }
+            }
+        }
+
+
+        //constructor
+        public Book(string t, string a, int total, string o)
+        {
+            title = t;
+            author = a;
+            totalPages = total;
+            owner = o;
+            timesRead = 0;
+        }
+
+        //methods
+        public void Print()
+        {
+            Console.WriteLine("Title :" + title);
+            Console.WriteLine("Author :" + author);
+            Console.WriteLine("Total Number of Pages: " + totalPages);
+            Console.WriteLine("Owner: " + owner);
+            Console.WriteLine("Times Read: " + timesRead);
+        }
+
+
+
+
+
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endVar = false;
+            Book theHobbit;
+
+            theHobbit = new Book(Console.ReadLine().Trim(), Console.ReadLine().Trim(),
+                int.Parse(Console.ReadLine().Trim()), Console.ReadLine().Trim());
+            //main loop
+            do
+            {
+                Console.WriteLine("What would you like to do?");
+                switch (Console.ReadLine().Trim().ToUpper())
+                {
+                    case "TITLE":
+                        Console.WriteLine(theHobbit.Title);
+                        continue;
+                    case "AUTHOR":
+                        Console.WriteLine(theHobbit.Author);
+                        continue;
+                    case "PAGES":
+                        Console.WriteLine(theHobbit.TotalPages);
+                        continue;
+                    case "OWNER":
+                        Console.WriteLine("Would you like to change the owner?");
+                        if (Console.ReadLine().Trim().ToLower() == "yes")
+                        {
+                            Console.WriteLine("Enter the new owner: ");
+                            theHobbit.Owner = Console.ReadLine();
+                            Console.WriteLine(theHobbit.Owner + " is the owner");
+                        }
+                        else
+                        {
+                            Console.WriteLine(theHobbit.Owner + " is the owner");
+                        }
+                        continue;
+                    case "READ":
+                        theHobbit.TimesRead++;
+                        Console.WriteLine("You have read the book " + theHobbit.TimesRead + " times.");
+                        continue;
+                    case "PRINT":
+                        theHobbit.Print();
+                        continue;
+                    case "DONE":
+                        endVar = true;
+                        continue;
+                    default:
+                        Console.WriteLine("Invalid Input, Try again.");
+                        continue;
+                }
+
+
+            }
+            while (endVar == false);
+        }
+    }
+}
