20ddbfe17bbbcaf8255f3f2250a6f119187ec840
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-11-27 09:21:40 -0500

    Signed-off-by: Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>

---
+++ BinIO/FileSplit/PlayerManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSplit
+{
+    class PlayerManager
+    {
+        //field
+        protected List<Player> players;
+
+        public PlayerManager()
+        {
+            players = new List<Player>();
+        }
+
+        //methods
+        public void CreatePlayer(string n, int s, int h)
+        {
+
+            players.Add(new Player(n, s, h));
+            Console.WriteLine("Player, " + n + " created...");
+        }
+
+        public void Print()
+        {
+            foreach (Player n in players)
+            {
+                Console.WriteLine(n.ToString());
+            }
+            if (players.Count == 0)
+            {
+                Console.WriteLine("There is no player data to print.");
+            }
+        }
+
+        public void Load()
+        {
+            try
+            {
+				Stream inStream = File.OpenRead("players.data");
+				BinaryReader input = new BinaryReader(inStream);
+
+                string[] values = new string[3];
+
+				int numToLoad = input.ReadInt32();
+
+				for (int i = 0; i < numToLoad; i++)
+				{
+					string name = input.ReadString();
+					int str = input.ReadInt32();
+					int hp = input.ReadInt32();
+					this.CreatePlayer(name, str, hp);
+				}
+				input.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void Save()
+        {
+            try
+            {
+				Stream outStream = File.OpenWrite("players.data");
+				BinaryWriter output = new BinaryWriter(outStream);
+				
+                if (players.Count == 0)
+                {
+                    Console.WriteLine("There is nothing to write, please create player data and try again");
+
+                }
+                else
+                {
+					output.Write(players.Count);
+                    foreach (Player n in players)
+                    {
+						output.Write(n.Name);
+						output.Write(n.Strength);
+						output.Write(n.Health);
+                    }
+                    output.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ BinIO/FileSplit/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSplit
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PlayerManager Control = new PlayerManager();
+			bool exitVar = true;
+			while (exitVar == true)
+			{
+				Console.Write("Choose one of the following options: \nCreate. Print. Save. Load. Info. Quit. \n>>");
+				string switcher = Console.ReadLine().Trim().ToLower();
+				if (switcher == "create")
+				{
+					Console.WriteLine("Please enter: ");
+					Console.Write("  Name: ");
+					string name = Console.ReadLine();
+					Console.Write("  Strength: ");
+					int str = int.Parse(Console.ReadLine());
+					Console.Write("  Health: ");
+					int health = int.Parse(Console.ReadLine());
+					Control.CreatePlayer(name, str, health);
+				}
+				else if (switcher == "print")
+				{
+					Control.Print();
+				}
+				else if (switcher == "save")
+				{
+					Control.Save();
+				}
+				else if (switcher == "load")
+				{
+					Control.Load();
+				}
+				else if (switcher == "quit")
+				{
+					System.Environment.Exit(0);
+				}
+				else if (switcher == "info")
+				{
+					if (File.Exists("players.data"))
+					{
+						Console.WriteLine("The file players.data was created at " + File.GetCreationTime("players.data"));
+						Console.WriteLine("The file players.data was last accessed at at " + File.GetLastAccessTime("players.data"));
+					}
+					else
+					{
+						Console.WriteLine("Please create players.data first");
+					}
+				}
+				else
+				{
+					Console.WriteLine("That's not a valid input please try again");
+				}
+			}
+
+		}
+	}
+}
