34d559231c8b9bb8c2770b574beb930242a8fd92
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-11-29 09:11:09 -0500

    Abstraction
    Signed-off-by: Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>

---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to dragon battle simulator 2034!");
+
+			int round = 0;
+			Random rng = new Random();
+
+			FrostDrgn nora = new FrostDrgn("Nora", 100, rng);
+			BlackDrgn reni = new BlackDrgn("Reni", 115, rng);
+
+			Console.WriteLine(nora.ToString());
+			Console.WriteLine(reni.ToString());
+
+
+			do
+			{
+				round++;
+				Console.WriteLine("Round " + round + ": -------------------------");
+				reni.TakeDamage(nora.Attack());
+				nora.TakeDamage(reni.Attack());
+
+				Console.WriteLine(reni.Name + " has " + reni.Health + " hp left");
+				Console.WriteLine(nora.Name + " has " + nora.Health + " hp left");
+				System.Threading.Thread.Sleep(1000);
+			}
+			while (nora.IsDead() && reni.IsDead());
+
+			if (nora.IsDead() && !reni.IsDead())
+			{
+				Console.WriteLine(nora.Name + " wins!");
+			}
+			else if (!nora.IsDead() && reni.IsDead())
+			{
+				Console.WriteLine(reni.Name + " wins!");
+			}
+			else
+			{
+				Console.WriteLine("Both contestants have been knocked out, it's a tie!");
+			}
+		}
+	}
+}
