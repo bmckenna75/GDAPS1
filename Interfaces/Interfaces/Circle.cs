f50bb6e7210702d9cebc2fbe37d7ac1a93d0bd6e
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-12-04 09:37:09 -0500

    Interfaces
    Signed-off-by: Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>

---
+++ Interfaces/Interfaces/Circle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+	class Circle : IPosition, IArea
+	{
+		//fields
+		protected double x;
+		public double X
+		{
+			get { return x; }
+			set { x = value; }
+		}
+		protected double y;
+		public double Y
+		{
+			get { return y; }
+			set { y = value; }
+		}
+		protected double area;
+		public double Area
+		{
+			get { return area; }
+		}
+		protected double perimeter;
+		public double Perimeter
+		{
+			get { return perimeter; }
+		}
+
+		//constructor
+		public Circle(double xValue, double yValue, double radius)
+		{
+			x = xValue;
+			y = yValue;
+			area = Math.PI * Math.Pow(radius, 2);
+			perimeter = Math.PI * radius * 2;
+		}
+
+		//methods
+		public double DistanceTo(IPosition position)
+		{
+			double distance;
+			distance = Math.Pow((Math.Pow(position.X - x, 2)) + Math.Pow(position.Y - y, 2), .5);
+			return distance;
+		}
+		public void MoveTo(double x, double y)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+		public void MoveBy(double xOffset, double yOffset)
+		{
+			x += xOffset;
+			y += yOffset;
+		}
+		public bool ContainsPosition(IPosition position)
+		{
+			if (this.DistanceTo(position) <= perimeter / (2 * Math.PI))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		public bool IsLargerThan(IArea areaToCheck)
+		{
+			if (areaToCheck.Area < area)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		public override string ToString()
+		{
+			return "This circle is centered on (" + x + ", " + y + ") and has an area of " + area + " with a perimeter of " + perimeter;
+		}
+
+	}
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Circle circle1 = new Circle(1, 1, 4);
+			Circle circle2 = new Circle(8, 5, 7);
+			Point point1 = new Point(1, 5);
+			Point point2 = new Point(4, 3);
+
+			//starting info
+			Console.WriteLine(circle1.ToString());
+			Console.WriteLine(circle2.ToString());
+			Console.WriteLine(point1.ToString());
+			Console.WriteLine(point2.ToString());
+
+			//moves everything
+			Console.WriteLine("\nMoving points and circles...\n\n");
+			circle1.MoveTo(2, 2);
+			circle2.MoveBy(-4, -2);
+			point1.MoveTo(1, 1);
+			point2.MoveBy(-4, -3);
+
+			//next info pannel
+			Console.WriteLine(circle1.ToString());
+			Console.WriteLine(circle2.ToString());
+			Console.WriteLine(point1.ToString());
+			Console.WriteLine(point2.ToString());
+
+			Console.WriteLine("The distance between the points is " + point1.DistanceTo(point2));
+			Console.WriteLine("The distance between the first point and the second circle is " + point1.DistanceTo(circle2));
+
+			if (circle1.IsLargerThan(circle2))
+			{
+				Console.WriteLine("\nThe first circle is larger than the second one");
+			}
+			else
+			{
+				Console.WriteLine("\nThe second circle is larger than the first one");
+			}
+
+			if (circle1.ContainsPosition(point1))
+			{
+				Console.WriteLine("Circle one contains point one");
+			}
+			if (circle2.ContainsPosition(point1))
+			{
+				Console.WriteLine("Circle two contains point one");
+			}
+			if (circle1.ContainsPosition(point2))
+			{
+				Console.WriteLine("Circle one contains point two");
+			}
+			if (circle2.ContainsPosition(point2))
+			{
+				Console.WriteLine("Circle two contains point two");
+			}
+
+
+
+
+
+
+
+
+		}
+	}
+}
