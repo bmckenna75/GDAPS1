380f1366e130175f24b06b6ca8685051c83a5e80
Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>
2017-12-06 09:43:59 -0500

    Last Practice exersize
    Signed-off-by: Bobby McKenna (RIT Student) <blm5582@ad.rit.edu>

---
+++ Enumeration/Enumeration/BakedGood.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+	public abstract class BakedGood
+	{
+		//fields
+		protected double price;
+		public double Price
+		{
+			get { return price; }
+		}
+	
+
+		//constructor
+		public BakedGood()
+		{
+			price = 0;
+		}
+
+		//methods
+		public abstract void PrintInformation();
+	}
+}
---
+++ Enumeration/Enumeration/Pie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+	class Pie : BakedGood
+	{
+		//field
+		protected string filling;
+
+		//constructor
+		public Pie(string fill)
+		{
+			price = 8;
+			filling = fill;
+		}
+
+		//methods
+		public override void PrintInformation()
+		{
+			Console.WriteLine("\n---Pie Order---\n" + filling + " pie\nTotal: " + price);
+		}
+	}
+}
